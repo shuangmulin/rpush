2c290fd0c490c1f7e7e57c0d4d19dcf17f3a0301
zhongbaolin <shuangmulin45@163.com>
2021-02-28 23:47:55 +0800

    Socket消息发送流程打通

--- rpush-route/src/main/java/com/regent/rpush/route/loadbalancer/MessageRequestInterceptor.java
+++ rpush-route/src/main/java/com/regent/rpush/route/loadbalancer/MessageRequestInterceptor.java
@@ -26,6 +26,7 @@ public class MessageRequestInterceptor implements RequestInterceptor {
             return;
         }
 
+        // 如果是消息推送，需要给接收端连接的服务端投放消息
         String body = new String(requestTemplate.body());
         JSONObject jsonObject = new JSONObject(body);
         QueryWrapper<RpushServerOnline> wrapper = new QueryWrapper<>();
