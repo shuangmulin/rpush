b92c0b242b58de05be9624f46783da40c6f4c87f
zhongbaolin <shuangmulin45@163.com>
2021-03-01 17:43:31 +0800

    加入服务监控

---
+++ rpush-monitor/src/main/java/com/regent/rpush/monitor/MonitorApplication.java
@@ -0,0 +1,15 @@
+package com.regent.rpush.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
