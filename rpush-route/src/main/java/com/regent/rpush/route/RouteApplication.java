3dfcc180bd837e390da33ae44f02a22863db68c2
zhongbaolin <shuangmulin45@163.com>
2021-03-07 22:36:05 +0800

    跨域问题

--- rpush-route/src/main/java/com/regent/rpush/route/RouteApplication.java
+++ rpush-route/src/main/java/com/regent/rpush/route/RouteApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableSwagger2Doc
 @SpringBootApplication(scanBasePackages = "com.regent.rpush")
@@ -16,6 +19,21 @@ public class RouteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RouteApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true)
+                        .maxAge(3600)
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 }
